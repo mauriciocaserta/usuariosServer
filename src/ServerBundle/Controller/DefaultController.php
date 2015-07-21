8012968262300c37351cc6405888d97c39e6e57d
mauricio <mauricio@squadit.com.br>
2015-07-21 18:52:21 -0300

    Inserido metodo de deletar usuario

--- src/ServerBundle/Controller/DefaultController.php
+++ src/ServerBundle/Controller/DefaultController.php
@@ -11,10 +11,10 @@ class DefaultController extends Controller {
         return $this->render('ServerBundle:Default:index.html.twig', array('name' => $name));
     }
 
-    public function cadastroAction() {       
+    public function cadastroAction() {
         $retorno = false;
         $mensagem = "";
-        
+
         $request = $this->getRequest();
 
         $nome = $request->get('nome');
@@ -25,24 +25,55 @@ class DefaultController extends Controller {
             $em = $this->getDoctrine()->getManager();
 
             try {
-              $usuario->setNome($nome);
-              $usuario->setSenha($senha);
-              
-              $em->persist($usuario);
-              $em->flush();
-              
-              $retorno = true;
-              $mensagem = "Usuario Inserido!";
+                $usuario->setNome($nome);
+                $usuario->setSenha($senha);
+
+                $em->persist($usuario);
+                $em->flush();
+
+                $retorno = true;
+                $mensagem = "Usuario Inserido!";
             } catch (Exception $e) {
                 $mensagem = $e->getMessage();
                 $retorno = false;
             }
         }
-        
+
         $rArr = array(
-            'retorno'=>$retorno
-            );
-        
+            'retorno' => $retorno
+        );
+
+        return new JsonResponse($rArr);
+    }
+
+    public function deleteAction() {
+        $mensagem = "";
+        $retorno = false;
+
+        $request = $this->getRequest();
+
+        $id = $request->request->get('id');
+
+        try {
+            $em = $this->getDoctrine()->getManager();
+
+            $entity = $em->getRepository('ServerBundle:Usuario')->find('id');
+
+            $em->remove($entity);
+            $em->flush();
+
+            $mensagem = "Deletado com Sucesso!";
+            $retorno = true;
+        } catch (Exception $e) {
+            $mensagem = $e->getMessage();
+            $retorno = false;
+        }
+
+        $rArr = array(
+            'mensagem' => $mensagem,
+            'retorno' => $retorno
+        );
+
         return new JsonResponse($rArr);
     }
 
